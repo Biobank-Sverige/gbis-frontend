2cc4e07c53336975d3e0fba686fbfc3f594eb7ec
Christian Johansen <Christian.Johansen@ucr.uu.se>
2020-02-17 15:41:55 +0100

    List all samples in local backend

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div>List of samples from backend server using GBIS 3.0</div>
+        <SampleList v-if="loaded" :samples="samples"></SampleList>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios'
+    import SampleList from './components/SampleList'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            SampleList
+        },
+        data: () => ({
+            loaded: false,
+            samples: null,
+        }),
+        async mounted() {
+            this.loaded = false;
+            const path = 'http://localhost:8080';
+            axios.get(path)
+                .then((res) => {
+                    console.log(res);
+                    this.samples = res.data;
+                    this.loaded = true;
+                })
+                .catch((error) => {
+                    // eslint-disable-next-line
+                    console.error(error);
+                });
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/SampleList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <table>
+            <thead>
+            <tr>
+                <td>Sample id</td>
+                <td>Sample study id</td>
+                <td>Sample usage</td>
+                <td>Last usage date</td>
+                <td>Donor</td>
+                <td>Parent sample id</td>
+                <td>Material type</td>
+                <td>Sample volume</td>
+                <td>Freeze/Thaw cycles</td>
+                <td>Plate id</td>
+                <td>Position in plate</td>
+            </tr>
+            </thead>
+        <tr v-for="sample in samples" v-bind:key="sample.sample_id">
+            <td>{{sample.sample_id}}</td>
+            <td>{{sample.study_id}}</td>
+            <td>{{sample.sample_usage}}</td>
+            <td>{{sample.last_usage_date}}</td>
+            <td>{{sample.donor}}</td>
+            <td>{{sample.parent_sample_id}}</td>
+            <td>{{sample.material_type}}</td>
+            <td>{{sample.sample_volume}}</td>
+            <td>{{sample.freeze_thaw_cycles}}</td>
+            <td>{{sample.sample_plate_id}}</td>
+            <td>{{sample.sample_plate_position}}</td>
+
+        </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['samples']
+    }
+</script>
