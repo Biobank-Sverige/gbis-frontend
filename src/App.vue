1371d19be7db47c1c0114a609cce62297206c585
Christian Johansen <Christian.Johansen@ucr.uu.se>
2020-02-18 11:20:13 +0100

    Added graph of samples collected over time

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,27 @@
     <div id="app">
         <div>List of samples from backend server using GBIS 3.0</div>
         <SampleList v-if="loaded" :samples="samples"></SampleList>
+        <div style="width:600px;height: 600px;">
+            <SampleGraph v-if="loaded" :chartdata="chartData"></SampleGraph>
+        </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios'
     import SampleList from './components/SampleList'
+    import SampleGraph from "./components/SampleGraph";
 
     export default {
         name: 'App',
         components: {
-            SampleList
+            SampleList,
+            SampleGraph
         },
         data: () => ({
             loaded: false,
             samples: null,
+            chartData: null
         }),
         async mounted() {
             this.loaded = false;
@@ -25,6 +31,12 @@
                 .then((res) => {
                     console.log(res);
                     this.samples = res.data;
+                    this.chartData = {
+                        datasets: [{
+                            label: "Samples collected per day",
+                            data: samplesPerDay(res.data)
+                        }]
+                    };
                     this.loaded = true;
                 })
                 .catch((error) => {
@@ -32,6 +44,17 @@
                     console.error(error);
                 });
         }
+    }
+
+    function samplesPerDay(samples) {
+        var counts = {};
+        samples.forEach(function (x) {
+            counts[x.sample_taken_date] = (counts[x.sample_taken_date] || 0) + 1;
+        });
+
+        return Object.keys(counts).map(function (key) {
+            return {"x": key, "y": counts[key]}
+        });
     }
 </script>
 
---
+++ src/components/SampleGraph.vue
@@ -0,0 +1,31 @@
+<script>
+    import { Line } from 'vue-chartjs'
+
+    export default {
+        extends: Line,
+        props: {
+            chartdata: {
+                type: Object,
+                default: null
+            },
+        },
+        data: ()=>({
+            options : {
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'day'
+                        }
+                    }]
+                }
+            }
+        }),
+        mounted () {
+            this.renderChart(this.chartdata, this.options)
+        }
+    }
+</script>
+
+<style>
+</style>
